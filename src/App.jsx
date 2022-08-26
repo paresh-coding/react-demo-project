ed4b7f488862b5ffc9cc7d3645b00d08fa09d260
Paresh Mohapatra <pareshliku@gmail.com>
2022-08-26 15:26:29 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,31 @@ import { Component, useEffect, useState } from 'react';
 // import './App.css';
 // import Form from 'react-jsonschema-form';
 // import FormModal from './formModal';
-import {FormBuilder, PredefinedGallery} from "@ginkgo-bioworks/react-json-schema-form-builder";
-import { mySchema1 } from './dataConfig';
+import { FormBuilder, PredefinedGallery } from "@ginkgo-bioworks/react-json-schema-form-builder";
+import { mySchema1, mySchema2,json1,json2 } from './dataConfig';
 // import moment from 'moment'
 // import { refeData } from './data'
 // import { mySchema1 } from './dataConfig';
 // mySchema1
+import Form from '@rjsf/core';
+import Modal from 'react-bootstrap/Modal';
+import mainBindFunction from './services';
 
 
 class App extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        schema: JSON.stringify(mySchema1),
-        uischema: '{}'
-      };
+        super(props);
+        this.state = {
+            schema: JSON.stringify(mySchema2),
+            uischema: '{}',
+            formData: {},
+            showForm: false,
+        };
     }
     render() {
-      return (
-        <div>
-          <FormBuilder
+        return (
+            <div>
+                <FormBuilder
             schema={this.state.schema}
             uischema={this.state.uischema}
             onChange={(newSchema, newUiSchema) => {
@@ -34,19 +39,43 @@ class App extends Component {
             }}
             
           />
-          <PredefinedGallery
-            schema={this.state.schema}
-            uischema={this.state.uischema}
-            onChange={(newSchema, newUiSchema) => {
-              this.setState({
-                schema: newSchema,
-                uischema: newUiSchema
-              })
-            }}
-          />
-          <button onClick={()=>{console.log(this.state.schema)}} >Submit</button>
-        </div>
-      );
+                {/* <PredefinedGallery
+                    schema={this.state.schema}
+                    uischema={this.state.uischema}
+                    onChange={(newSchema, newUiSchema) => {
+                        this.setState({
+                            schema: newSchema,
+                            uischema: newUiSchema
+                        })
+                    }}
+                /> */}
+
+
+                <button onClick={() => {
+                    console.log(mainBindFunction(json2,mySchema2))
+                   this.setState({
+                    schema: JSON.stringify(mainBindFunction(json2,mySchema2))  
+                   })                    
+                    this.state.showForm = true }} >Preview</button>
+
+                <Modal show={this.state.showForm} onHide={() => { this.state.showForm = false }} size="Xl" className="Modal-Container" >
+                    <Modal.Body  >
+
+                        <Form
+                            schema={JSON.parse(this.state.schema)}
+                            uiSchema={JSON.parse(this.state.uischema)}
+                            onChange={(newFormData) => this.setState({ formData: newFormData.formData })}
+                            formData={this.state.formData}
+                            submitButtonMessage={"Submit"}
+                            onSubmit=(()=>{})
+                        />
+                    </Modal.Body>
+                </Modal >
+
+
+            </div>
+
+        );
     }
-  }
+}
 export default App
---
+++ src/services.js
@@ -0,0 +1,51 @@
+function mainBindFunction(mainObj, schema) {
+
+    recurObject(schema);
+
+    function recurObject(recObj) {
+        for (var schemaProps in recObj) {
+            if (typeof recObj[schemaProps] === "object") {
+                let obj1 = recObj[schemaProps];
+                if (recObj[schemaProps].hasOwnProperty("type") && schemaProps !== "items" && schemaProps !== "default") {
+                    recObj[schemaProps] = {
+                        ...recObj[schemaProps],
+                        default: dynamicBindingData(schemaProps, recObj[schemaProps]['type'])[0] || ''
+                    }
+                }
+                recurObject(recObj[schemaProps])
+            }
+        }
+    }
+
+    function dynamicBindingData(schemaProps, type) {
+        let data = []
+        secondRecursionObj(mainObj);
+
+        function secondRecursionObj(obj) {
+            for (var mainObjProps in obj) {
+                if (typeof obj[mainObjProps] === "object") {
+                    if (mainObjProps === schemaProps) {
+                        if (type === "array") {
+                            data.push(obj[mainObjProps][0])
+                        }
+                    } else {
+                        secondRecursionObj(obj[mainObjProps])
+                    }
+                } else if (typeof obj[mainObjProps] === "array") {
+                    if (mainObjProps === schemaProps) {
+                        data.push(obj[mainObjProps][0])
+                    }
+                } else if (typeof obj[mainObjProps] === "string") {
+                    if (mainObjProps === schemaProps) {
+                        data.push(obj[mainObjProps])
+                    }
+                }
+            }
+        }
+        return data
+    }
+
+    return schema
+}
+
+export default mainBindFunction
